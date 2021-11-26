d7e76dda717ea73f4fb31b21f96a4f6a410bd709
JSONRete <jsonrete@gmail.com>
2021-11-25 23:59:46 -0500

    changed table name 'numbers' to 'number'

--- db/migrate/20211126045201_create_producers.rb
+++ db/migrate/20211126045201_create_producers.rb
@@ -2,7 +2,7 @@ class CreateProducers < ActiveRecord::Migration[6.1]
   def change
     create_table :producers do |t|
       t.string :name
-      t.numbers :raiting
+      t.number :raiting
       t.string :search
 
       t.timestamps
