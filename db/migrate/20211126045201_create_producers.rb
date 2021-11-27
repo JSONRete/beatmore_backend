37b0f16ae8c938e2e640bcafd357d01c58be482a
JSONRete <jsonrete@gmail.com>
2021-11-26 23:44:16 -0500

    changed table name from 'raiting' to 'rating'

--- db/migrate/20211126045201_create_producers.rb
+++ db/migrate/20211126045201_create_producers.rb
@@ -2,7 +2,7 @@ class CreateProducers < ActiveRecord::Migration[6.1]
   def change
     create_table :producers do |t|
       t.string :name
-      t.integer :raiting
+      t.integer :rating
       t.string :search
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_045201) do
 
   create_table "producers", force: :cascade do |t|
     t.string "name"
-    t.integer "raiting"
+    t.integer "rating"
     t.string "search"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
