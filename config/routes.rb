ba1f823bccc4c4b52018304bcb38618aff440fd0
JSONRete <jsonrete@gmail.com>
2021-12-02 23:58:37 -0500

    working on route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :producers
+  # post "producer_pull", to: “action#producers"
   # resources :producers
   resources :beats
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
