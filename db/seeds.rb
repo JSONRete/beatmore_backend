8f6e6cc945516a5231365c3df8264d4c4c639267
JSONRete <jsonrete@gmail.com>
2021-12-02 23:59:49 -0500

    put seed practice in controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+# require 'rubygems'
+# require 'httparty'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +10,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Beat.create([
-#     {artist: 'Mobb Deep', song: 'Shook Ones Part II', producer: 'Havoc', media: 'https://youtu.be/0NUX4tW5pps'},
+#     {artist: 'Mobb Deep', song: 'Shook Ones Part II', producer: 'Havoc', media: 'https://youtu.be/v9MILl7AMLo'},
 #     {artist: 'Wu-Tang Clan', song: 'C.R.E.A.M.', producer: 'RZA', media: 'https://youtu.be/HEhS6wtURB0'},
 #     {artist: 'A Tribe Called Quest featuring Leaders of the New School', song: 'Scenario', producer: 'A Tribe Called Quest', media: 'https://youtu.be/g2mXc8GNLHM'},
 #     {artist: '50 Cent', song: 'In da Club', producer: 'Dr. Dre', media: 'https://youtu.be/s7q4_dnzw68'},
@@ -21,6 +24,41 @@
 #     {artist: 'Fabolous', song: 'Breathe', producer: 'Just Blaze', media: 'https://youtu.be/QDNswWBG0PM'}
 # ])
 # data = HTTParty.get('https://musicbrainz.org/ws/2/artist?query=kanye-west&fmt=json&limit=1')
-data = HTTParty.get('http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json')
+# data = HTTParty.get('http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json')
+# data = HTTParty.get('https://musicbrainz.org/ws/2/artist?query=kanye-west&fmt=json&limit=1?token=Swv62vvxcUNH0vOdLDTTJzIXokR4HQvwIdkFI3xJ')
 # http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases
+
+# data = HTTParty.get('https://musicbrainz.org/ws/2/artist?query=kanye-west&fmt=json&limit=1', {
+#     headers: {"User-Agent" => "Httparty"},
+#     debug_output: STDOUT, # To show that User-Agent is Httparty
+#   })
+
+# MusicBrainz.configure do |c|
+#   # Application identity (required)
+#   c.app_name = "My Music App"
+#   c.app_version = "1.0"
+#   c.contact = "support@mymusicapp.com"
+
+#   # Cache config (optional)
+#   c.cache_path = "/tmp/musicbrainz-cache"
+#   c.perform_caching = true
+
+#   # Querying config (optional)
+#   c.query_interval = 1.2 # seconds
+#   c.tries_limit = 2
+# end
+
+# require 'musicbrainz'
+
+# Search for artists
+# @suggestions = MusicBrainz::Artist.search("Jet")
+
+# Find artist by name or mbid
+# @foo_fighters = MusicBrainz::Artist.find_by_name("Foo Fighters")
+# @kasabian = MusicBrainz::Artist.find("69b39eab-6577-46a4-a9f5-817839092033")
+
+# # Use them like ActiveRecord models
+# @empire_tracks = @kasabian.release_groups[8].releases.first.tracks
+
+
 binding.pry
