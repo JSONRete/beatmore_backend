4d81c41c579d5467c3673dc3f69cf56eb0aa464a
JSONRete <jsonrete@gmail.com>
2021-12-07 23:33:31 -0500

    implemented custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :producers
-  # post "producer_pull", to: “action#producers"
+    get 'producer_pull', to: 'producers#create_prod'
+
   # resources :producers
   resources :beats
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
