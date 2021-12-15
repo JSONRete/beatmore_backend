3e70f876b5a9d1fe40bc77ef325f39ea26735dfb
JSONRete <jsonrete@gmail.com>
2021-12-14 23:15:36 -0500

    updated migration file

--- db/migrate/20211130180426_create_producers.rb
+++ db/migrate/20211130180426_create_producers.rb
@@ -2,9 +2,8 @@ class CreateProducers < ActiveRecord::Migration[6.1]
   def change
     create_table :producers do |t|
       t.string :name
-      t.string :gender
+      t.string :legal_name
       t.string :begin_area
-      t.string :area
       t.string :birthday
 
       t.timestamps
