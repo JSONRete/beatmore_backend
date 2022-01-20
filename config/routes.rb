4e33c95dd681e8c61d94d0737ffea3979781e473
JSONRete <jsonrete@gmail.com>
2022-01-19 22:45:58 -0600

    tightened and cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :producers
     get 'producer_pull', to: 'producers#create_prod'
-
-  # resources :producers
   resources :beats
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
